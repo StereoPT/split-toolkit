496ac0c953845f05945cb2dbcd4dc0761122362f
JoaoSaIvador <j.salvador@kigroup.de>
2023-01-05 16:00:34 +0000

    fix: double alerts and publisher

--- src/extension.ts
+++ src/extension.ts
@@ -3,36 +3,54 @@ import * as vscode from 'vscode';
 const setupTerminal = () => {
 	let splitTerminal = vscode.window.activeTerminal;
 
-	if(!splitTerminal) {
-		splitTerminal = vscode.window.createTerminal("Split");
+	if (!splitTerminal) {
+		splitTerminal = vscode.window.createTerminal('Split');
 		splitTerminal.show();
 	}
 
 	return splitTerminal;
-}
+};
 
 export function activate(context: vscode.ExtensionContext) {
-
 	const SplitUP = () => {
 		const splitTerminal = setupTerminal();
+		const commands = [
+			'colima start',
+			'docker-compose up -d mongo mongo2 redis',
+			'exit',
+		];
 
 		// Check before things START to avoid Double Up
-		splitTerminal.sendText('colima start');
-		splitTerminal.sendText('docker-compose up -d mongo mongo2 redis');
-		vscode.window.showInformationMessage('Split Up!');
-	}
+		splitTerminal.sendText(commands.join(' && '));
+		const disposable = vscode.window.onDidCloseTerminal(t => {
+			vscode.window.showInformationMessage('Split Up!');
+			disposable.dispose();
+		});
+	};
 
 	const SplitDOWN = () => {
 		const splitTerminal = setupTerminal();
-		
+		const commands = [
+			'docker-compose stop mongo mongo2 redis',
+			'colima stop',
+			'exit',
+		];
+
 		// Check before things STOP to avoid Double Down
-		splitTerminal.sendText('docker-compose stop mongo mongo2 redis');
-		splitTerminal.sendText('colima stop');
-		vscode.window.showInformationMessage('Split Down!');
-	}
-	
-	context.subscriptions.push(vscode.commands.registerCommand('split-toolkit.split-up', SplitUP));
-	context.subscriptions.push(vscode.commands.registerCommand('split-toolkit.split-down', SplitDOWN));
+		splitTerminal.sendText(commands.join(' && '));
+
+		const disposable = vscode.window.onDidCloseTerminal(t => {
+			vscode.window.showInformationMessage('Split Down!');
+			disposable.dispose();
+		});
+	};
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('split-toolkit.split-up', SplitUP)
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('split-toolkit.split-down', SplitDOWN)
+	);
 }
 
 export function deactivate() {}
